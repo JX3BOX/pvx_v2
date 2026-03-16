4eef59b6f193395917554f1906cf3714089911c6
iRuxu <rx6@qq.com>
2026-03-16 20:48:47 +0800

    fix:vue3

---
+++ src/views/furniture/Furniture.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="app">
+        <CommonHeader></CommonHeader>
+        <!-- <Breadcrumb name="家具大全" slug="furniture" root="/furniture" :feedbackEnable="true" :crumbEnable="false">
+            <img slot="logo" svg-inline :src="getAppIcon('furniture')" />
+        </Breadcrumb> -->
+        <Nav @statusChange="statusChange"></Nav>
+        <Main :class="navStatusClass" :withoutRight="true" :withoutLeft="true" :withoutBread="true">
+            <div class="m-main">
+                <keep-alive include="FurnitureList">
+                    <router-view></router-view>
+                </keep-alive>
+            </div>
+            <!-- <Footer></Footer> -->
+        </Main>
+    </div>
+</template>
+
+<script>
+import Nav from "@/components/Nav_v5.vue";
+// import { getAppIcon } from "@jx3box/jx3box-common/js/utils";
+import { __imgPath, __dataPath } from "@/utils/config";
+export default {
+    name: "Furniture",
+    props: [],
+    data: function () {
+        return {
+            navStatusClass: "is-regular",
+        };
+    },
+    provide: {
+        __imgRoot: __imgPath + "homeland/",
+        __dataRoot: __dataPath + "pvx/",
+    },
+    computed: {},
+    methods: {
+        // getAppIcon,
+        statusChange(navStatusClass) {
+            this.navStatusClass = navStatusClass;
+        },
+    },
+    components: { Nav },
+};
+</script>
+
+<style lang="less">
+@import "~@/assets/css/app.less";
+</style>
